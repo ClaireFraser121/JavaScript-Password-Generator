4658354e5997e89f91e72fee28defbc2fa9ef341
Claire Fraser <clairefraser121@gmail.com>
2023-10-28 23:09:04 +0100

    made alert message more user friendly

--- asssets/js/script.js
+++ asssets/js/script.js
@@ -31,30 +31,35 @@ function getPasswordOptions() {
 
   // Add an event listener to confirm options and generate the password
   optionsConfirmBtn.addEventListener('click', () => {
-      const length = parseInt(passwordLengthInput.value);
-      const hasLower = includeLowerInput.checked;
-      const hasUpper = includeUpperInput.checked;
-      const hasNumbers = includeNumbersInput.checked;
-      const hasSpecial = includeSpecialInput.checked;
+    const length = parseInt(passwordLengthInput.value);
+    const hasLower = includeLowerInput.checked;
+    const hasUpper = includeUpperInput.checked;
+    const hasNumbers = includeNumbersInput.checked;
+    const hasSpecial = includeSpecialInput.checked;
 
-      if (
-          isNaN(length) ||
-          length < 8 ||
-          length > 128 ||
-          (!hasLower && !hasUpper && !hasNumbers && !hasSpecial)
-      ) {
-          alert('Invalid options. Please try again.');
-      } else {
-          // Close the modal
-          optionsModal.style.display = 'none';
-          // Generate the password based on the selected options
-          generatePassword({ length, hasLower, hasUpper, hasNumbers, hasSpecial });
-      }
+    if (
+      isNaN(length) ||
+      length < 8 ||
+      length > 128 ||
+      (!hasLower && !hasUpper && !hasNumbers && !hasSpecial)
+    ) {
+      // Replace the existing alert with a more user-friendly error message
+      alert('Please check your options:\n\n' +
+        '- Password length must be between 8 and 128 characters.\n' +
+        '- At least one option (lowercase, uppercase, numbers, or special characters) must be selected.\n\n' +
+        'Please try again.');
+
+    } else {
+      // Close the modal
+      optionsModal.style.display = 'none';
+      // Generate the password based on the selected options
+      generatePassword({ length, hasLower, hasUpper, hasNumbers, hasSpecial });
+    }
   });
 
   // Add an event listener to close the modal when the "×" button is clicked
   document.querySelector('.close-button').addEventListener('click', () => {
-      optionsModal.style.display = 'none';
+    optionsModal.style.display = 'none';
   });
 }
 
@@ -71,25 +76,25 @@ function generatePassword(options) {
   const possibleCharacters = [];
 
   if (hasLower) {
-      possibleCharacters.push(...lowerCasedCharacters);
+    possibleCharacters.push(...lowerCasedCharacters);
   }
 
   if (hasUpper) {
-      possibleCharacters.push(...upperCasedCharacters);
+    possibleCharacters.push(...upperCasedCharacters);
   }
 
   if (hasNumbers) {
-      possibleCharacters.push(...numericCharacters);
+    possibleCharacters.push(...numericCharacters);
   }
 
   if (hasSpecial) {
-      possibleCharacters.push(...specialCharacters);
+    possibleCharacters.push(...specialCharacters);
   }
 
   let password = '';
 
   for (let i = 0; i < length; i++) {
-      password += getRandom(possibleCharacters);
+    password += getRandom(possibleCharacters);
   }
 
   // Set the generated password in the textarea
